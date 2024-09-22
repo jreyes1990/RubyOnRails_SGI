ad847ceb4fea5736c831f3b60431cd88a7a73c73
jreyes1990 <jarl1990mt@gmail.com>
2024-09-21 22:38:44 -0600

    Change 21092024_2240 Configuración deploy production

--- config/puma.rb
+++ config/puma.rb
@@ -9,12 +9,12 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
+# Puerto en el que Puma escuchará (normalmente 3000 para producción con Nginx)
 port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+# Asegúrate de que el entorno esté configurado correctamente
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -24,15 +24,21 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# Configura el número de workers para ejecutar en modo clúster (ajusta según tu servidor)
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
-#
-# preload_app!
+# Preload App para aprovechar Copy-On-Write
+preload_app!
+
+# Bloque necesario para asegurar que los workers se conecten correctamente a la base de datos
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+# end
 
 # Allow puma to be restarted by `rails restart` command.
+# Reiniciar Puma con el comando `rails restart`
 plugin :tmp_restart
